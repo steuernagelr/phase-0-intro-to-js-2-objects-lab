237985fca08f09f9243e8fa343062a1604d1abe7
Rielly Steuernagel <reiguy@comcast.net>
2021-07-07 18:50:38 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newObject = { ...employee }
+
+    newObject[key] = value
+
+    return newObject
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, value, key) {
+    employee[value] = key
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newObject = Object.assign({}, employee)
+    delete newObject[key]
+    return newObject
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    employee[key] = undefined
+    return employee
+}
